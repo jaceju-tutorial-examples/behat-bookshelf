1546271f439edb7f5751170d06773e1a11e9404d
Jace Ju <jaceju@gmail.com>
2015-09-02 00:43:48 +0800

    Add step definitions

--- features/bootstrap/BookshelfContext.php
+++ features/bootstrap/BookshelfContext.php
@@ -1,8 +1,50 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
 use Goez\BehatLaravelExtension\Context\LaravelContext;
 
 class BookshelfContext extends LaravelContext
 {
     use Authentication;
+
+    /**
+     * @Given 用帳號 :name :email 登入系統
+     */
+    public function iHaveLoggedInAs($name, $email)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given 書架上現有書籍
+     */
+    public function onShelfBooks(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given 書籍 :bookName 已被 :email 借出
+     */
+    public function bookCheckedOutByUser($bookName, $email)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When 進入首頁
+     */
+    public function visitHome()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then 顯示書籍清單、出借狀況
+     */
+    public function booksList(TableNode $table)
+    {
+        throw new PendingException();
+    }
 }
