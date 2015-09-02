61bf3c546efe9c946fc853a9bae29f1054dfc560
Jace Ju <jaceju@gmail.com>
2015-09-02 16:43:15 +0800

    Create steps

--- features/bootstrap/MembershipContext.php
+++ features/bootstrap/MembershipContext.php
@@ -1,7 +1,31 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Goez\BehatLaravelExtension\Context\LaravelContext;
 
 class MembershipContext extends LaravelContext
 {
+    /**
+     * @When 註冊帳號 :name :email
+     */
+    public function iRegisterAccount($name, $email)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then 登入系統
+     */
+    public function iHaveLoggedIn()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then 導向首頁
+     */
+    public function iBeRedirectedHome()
+    {
+        throw new PendingException();
+    }
 }
