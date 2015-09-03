95aec8d42433bc2906ab153b45e8e8e6ea2a6816
Jace Ju <jaceju@gmail.com>
2015-09-03 18:05:58 +0800

    Add authentication checking of bookshelf

--- app/Http/Controllers/BookshelfController.php
+++ app/Http/Controllers/BookshelfController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Auth;
 
 class BookshelfController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $books = Book::all();
