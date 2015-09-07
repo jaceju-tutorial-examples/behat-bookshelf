ee8fdbc73b460f7c2bbb966f13b8b37e4a45c184
Jace Ju <jaceju@gmail.com>
2015-09-07 22:42:50 +0800

    Add test case of index method

--- tests/BookshelfControllerTest.php
+++ tests/BookshelfControllerTest.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BookshelfController;
 use App\Services\BookshelfService;
+use Illuminate\View\View;
 
 class BookshelfControllerTest extends TestCase
 {
@@ -16,5 +17,28 @@ class BookshelfControllerTest extends TestCase
         // 通常 controller 有 constructor 的話，就可以考慮把它放在 setUp 裡
         $this->service = Mockery::mock(BookshelfService::class);
         $this->controller = new BookshelfController($this->service);
+    }
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        // Arrange
+        $expected = [1, 2, 3, 4, 5];
+        $this->service->shouldReceive('getAllBooks')
+            ->once()
+            ->withNoArgs()
+            ->andReturn($expected);
+
+        // Act
+        $view = $this->controller->index();
+        /** @var View $view */
+        $actual = $view->books;
+
+        // Assert
+        $this->assertEquals($expected, $actual);
     }
 }
