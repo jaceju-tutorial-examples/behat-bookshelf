d450d9a2a3bb4e4f0a2cedeb8637aeb548ad21d8
Jace Ju <jaceju@gmail.com>
2015-09-08 17:41:23 +0800

    Add test case for get all books

--- tests/BookshelfServiceTest.php
+++ tests/BookshelfServiceTest.php
@@ -1,9 +1,51 @@
 <?php
 
+use App\Book;
+use App\Services\BookshelfService;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BookshelfServiceTest extends TestCase
 {
     use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @var BookshelfService
+     */
+    protected $service;
+
+    /**
+     * 不能用 setUp ，因為會在 DatabaseMigrations 前先執行而導致錯誤
+     */
+    protected function initFixtures()
+    {
+        $this->user = factory(User::class)->make([
+            'id' => 1,
+            'name' => 'Jace Ju',
+            'email' => 'jaceju@example.com',
+        ]);
+
+        $book = factory(Book::class)->make();
+
+        $this->service = new BookshelfService($this->user, $book);
+    }
+
+    public function testGetAllBooks()
+    {
+        // Arrange
+        $this->initFixtures();
+        factory(Book::class)->times(10)->create();
+
+        // Act
+        $books = $this->service->getAllBooks();
+
+        // Assert
+        $this->assertCount(10, $books);
+    }
 }
